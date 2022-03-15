b752b477add3f8893ff641785ff1cc85ce6107db
Marothi Mahlake <code_artist@outlook.com>
2022-03-15 13:26:22 +0200

    Node.js transcription demo application completed with just speech to text functionality.

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
+require("dotenv").config(); // Enable dotenv to access environment variables in the .env file.
 const fs = require("fs");
-const sdk = require("microsoft-cognitive-services-speech-sdk");
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+
+const speechConfig = sdk.SpeechConfig.fromSubscription(
+  process.env.SUBSCRIPTION_KEY, // Subscription Key
+  process.env.SERVICE_REGION // Service Region
+);
+
+speechConfig.speechRecognitionLanguage = "en-ZA"; // Set the speech recognition language.
+
+/**
+ * Transcribes speech audio to text from the file specified within the function.
+ */
+function transcribeFromFile() {
+  let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync("Obnoxous.wav"));
+  let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+
+  speechRecognizer.recognizeOnceAsync((result) => {
+    switch (result.reason) {
+      case sdk.ResultReason.RecognizedSpeech:
+        console.log(`RECOGNIZED: Text=${result.text}`);
+        break;
+      case sdk.ResultReason.NoMatch:
+        console.log("NOMATCH: Speech could not be recognized.");
+        break;
+      case sdk.ResultReason.Canceled:
+        const cancellation = CancellationDetails.fromResult(result);
+        console.log(`CANCELED: Reason=${cancellation.reason}`);
+
+        if (cancellation.reason == sdk.CancellationReason.Error) {
+          console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+          console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+          console.log("CANCELED: Did you update the key and location/region info?");
+        }
+        break;
+    }
+    speechRecognizer.close();
+  });
+}
+
+transcribeFromFile();
